33f37ccd8085fe4cd4e8e64e785b805c2147382f
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-08 17:09:55 +0100

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - printf this "with proper grammar, but the outcome is a piece of art,'
+ * Return: 0 if the program excutes correctly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n);
