e824f0fc3dbc94aaaf6113df0d017c5a35f57447
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-09 21:22:48 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Print hexadecimal value,
+ *Return: 0 on SUCCESS
+ */
+int main(void)
+{
+	int i;
+	char chr;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	for (ch = 'a'; ch < 'g')
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
