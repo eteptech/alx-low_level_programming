8fe879f277e4bfd14d6424cb411897448d81da18
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-13 17:03:07 +0100

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,11 +4,11 @@
  *@num : number to check
  *Return:0 or 1
  */
-int print_last_digit(int num)
+int print_last_digit(int n)
 {
-	num = num >= 0 ? num : num * -1;
-	int last_num = num % 10;
+	n = n >= 0 ? n : n * -1;
+	int last = n % 10;
 
-	_putchar('0' + last_num);
-	return (last_num);
+	_putchar('0' + last);
+	return (last);
 }
