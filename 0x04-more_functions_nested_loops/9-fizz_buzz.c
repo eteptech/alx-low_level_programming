2d4525b7eb20f451332ed33487b3e705a72e6f55
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-15 19:22:16 +0100

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,32 +9,26 @@ int main(void)
 {
 	int i;
 
-	i = 1;
-	while (i < 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Buzz");
-		}
-		else
+			printf(" FizzBuzz");
+		} else if (i == 1)
 		{
 			printf("%d", i);
-		}
-		if (i != 100)
+		} else
 		{
-			putchar(' ');
-
+			printf(" %d", i);
 		}
-
-		i++;
 	}
-	putchar('\n');
+	printf("\n");
+
+	return (0);
 }
