afd1f26cf40110b6827a85f815226574b1a36669
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-15 15:15:28 +0100

    6-print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,13 @@
 #include "main.h"
-
 /**
  *print_line- Prints a straing line
+ *@n : lenght of the line to be drawn
  *Return: void
  */
 void print_line(int n)
 {
 	int i;
-	
+
 	for (i = 1; i <= n ; i++)
 	{
 		if (n == 0 || n < 0)
