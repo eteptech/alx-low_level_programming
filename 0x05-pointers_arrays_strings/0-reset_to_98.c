435c8095bcf76355afa94d6d657a34cd9e3a4e0b
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-19 21:03:27 +0100

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98- prints pointer variable
+ *@n : a pointer the integer we want to set to 98
+ *Return: void
+ */
+void reset_to_98(int *n)
+{
+	int *p;
+
+	*p = &n;
+	*p = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int putchar(char ch);
+void reset_to_98(int *n);
+
+
+#endif
