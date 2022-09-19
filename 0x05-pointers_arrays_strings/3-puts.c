6e0d7c38157a0c580abf985dc4450fba5b8c1248
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-19 23:04:09 +0100

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,14 +9,10 @@ void _puts(char *str)
 {
 	int i;
 
-	i = 0;
-	for (i = 0; i < sizeof(str) / sizeof(str[0]); i++)
+	while (str[i] != '\0')
 	{
-		while (str[i] != '\0')
-		{
-			_putchar(str[i]);
-			i++;
-		}
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
