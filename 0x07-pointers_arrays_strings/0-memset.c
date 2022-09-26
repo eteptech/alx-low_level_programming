6b9b378e41b26b364b260c2894ce73981cb1a9c7
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-26 15:59:38 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int x;
 
-	x = 0;
-	while (x < n)
+	
+	for (x = 0; x < n; x++; n--)
 	{
 		s[x] = b;
-		x++;
-		n--;
 	}
 	return (s);
 }
