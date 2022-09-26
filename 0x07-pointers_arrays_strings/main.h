66008abbad07a91bbf00f24aba47f2cb559ee641
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-26 14:51:30 +0100

    File:added 1-memcpy.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,8 +4,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+
 char *_strchr(char *s, char c);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char ch);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
