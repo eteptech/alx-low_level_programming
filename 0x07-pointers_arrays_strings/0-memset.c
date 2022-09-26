591bd106034306d80195807e9471cf62fe5d9c51
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-09-26 14:37:00 +0100

    file: added 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset_memset- prints the maching charater
+ *_memset- prints the maching charater
  *@s :first param
  *@b :second param
  *@n :number of occurrence
@@ -10,6 +10,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int x;
+
 	while (x < n)
 	{
 		s[x] = b;
