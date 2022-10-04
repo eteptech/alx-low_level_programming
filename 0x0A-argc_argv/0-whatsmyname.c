7e4e02ecb43156badb6f1d97c69c059e17be6654
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-10-04 15:39:01 +0100

    Changed betty coding style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main- prints the name of the program
- * @argc : No. of arguement counts
- * @argv : number of character enter
+ *main - prints the name of the program
+ *@argc : No. of arguement counts
+ *@argv : number of character enter
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
