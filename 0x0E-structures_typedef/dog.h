198d869a0d40bd9a3aaffea69b8a45d8dddb9582
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-10-10 20:49:55 +0100

    File: added 2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,4 +24,5 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
