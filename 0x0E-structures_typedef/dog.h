bcf12a07a5bbf6e381a8f98de7beaa6fdb19bf8e
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2022-10-10 15:47:01 +0100

    change betty coding style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,11 +3,13 @@
  * @name : Dogs name
  * @age : Dogs age
  * @owner : Who owns a particular dog
+ * Description: This struct take dogs name, age and owner
  */
 #include <stdio.h>
+
 typedef struct dog
 {
 	char *name;
 	int age;
 	char *owner;
-}
+};
